9883003d45c3397ada3ac239b4ee6089288d12f2
Alexander Walter <alexander@wackazong.com>
2024-06-20 13:57:31 +0200

    improve example

--- macro-test/src/lib.rs
+++ macro-test/src/lib.rs
@@ -2,11 +2,13 @@ use proc_macro::my_macro;
 
 #[my_macro]
 pub fn my_func() {
-    MyEnum::MyVariant {
-        my_field: "".into(),
-    };
+    let my_field = String::default();
+    let _this_is_fine = MyEnum::MyVariant { my_field };
+    let my_var = String::default();
+    let _this_produces_a_parse_error = MyEnum::MyVariant { my_field: my_var };
 }
 
+#[allow(dead_code)]
 enum MyEnum {
     MyVariant { my_field: String },
 }
--- proc-macro/src/lib.rs
+++ proc-macro/src/lib.rs
@@ -4,17 +4,19 @@ use syn::parse::{Parse, ParseStream};
 use syn::parse_macro_input;
 use syn::*;
 
+#[allow(dead_code)]
 #[proc_macro_attribute]
 pub fn my_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
     parse_macro_input!(input as MyInput);
     quote! {}.into()
 }
 
+#[allow(dead_code)]
 struct MyInput {}
 
 impl Parse for MyInput {
     fn parse(input: ParseStream) -> Result<Self> {
-        let _ = input.parse::<ItemFn>();
+        input.parse::<ItemFn>()?;
         Ok(Self {})
     }
 }
